2175f9dd8a883d39381a25c5d8d00d48b8ac2f01
Yuri <yuri.pichugin@yandex.ru>
2021-01-20 15:19:51 +0300

    fix html names

--- test_site_content.sh
+++ test_site_content.sh
@@ -3,8 +3,8 @@
 wget -O page_from_git.html https://raw.githubusercontent.com/Pecha99/content_repo/master/content.html
 wget -O page_from_container.html http://178.154.232.99:1234/content.html
 
-VAR1=curl page_from_git
-VAR2=curl page_from_container
+VAR1=curl page_from_git.html
+VAR2=curl page_from_container.html
 
 if [ "$VAR1" = "$VAR2" ]; then
     echo "Test passed"
