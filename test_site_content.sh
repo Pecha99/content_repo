e0787cf16c5fde43379fc37a5886018b3660455e
Yuri <yuri.pichugin@yandex.ru>
2021-01-20 16:13:44 +0300

    fixed brokern link in test script

--- test_site_content.sh
+++ test_site_content.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O page_from_git.html https://raw.githubusercontent.com/Pecha99/content_repo/master/index.html
+wget -O page_from_git.html https://raw.githubusercontent.com/Pecha99/content_repo/master/content.html
 wget -O page_from_container.html http://178.154.232.99:1234/content.html
 
 if cmp -s page_from_git.html page_from_container.html; then
